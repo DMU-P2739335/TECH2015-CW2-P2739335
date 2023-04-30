738f1bceefcf63c842395a9f254c0953bc9b074c
DMU-P2739335 <p2739335@my365.dmu.ac.uk>
2023-04-30 12:41:50 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+//Function for the directing to the other pages.
+
+function redirectToMenuPage() {
+  window.location.href="/menu/menu.html";  
+}
+function redirectToPhotoGalleryPage() {
+window.location.href="/photogallery/photogallery.html";  
+}
+function redirectToMusicVideoPage() {
+window.location.href="/musicvideo/musicvideo.html";  
+}
+function redirectToMoviesPage() {
+window.location.href="/movies/movie.html";  
+}
+function redirectToDiscographyPage() {
+window.location.href="/discography/discography.html";  
+}
+function redirectToBiographyPage() {
+window.location.href="/biography/biography.html";  
+}
